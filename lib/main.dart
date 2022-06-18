e541a3dd4e2a3c1fbff9b9ef5fca6e710c302cfd
GenkiSugiyama <sgym0318marcos@gmail.com>
2022-06-18 16:09:04 +0900

    チュートリアル完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -13,20 +11,55 @@ class MyApp extends StatelessWidget {
   // buildメソッドでウィジェットのUIを作成する
   @override
   Widget build(BuildContext context){
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       // Scaffoldウィジェット：アプリの基盤となるUIを作成する
-      home: Scaffold(
-        // appBarプロパティ：画面上部のトップバーのUIを宣言
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-          backgroundColor: Colors.pink,
-        ),
-        // bodyプロパティ：メインコンテンツとなるウィジェットを宣言（ここではCenterウィジェット）
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+      home: RandomWords()
+    );
+  }
+}
+
+// StatefulWidgetを継承するとリロードしなくてもユーザー操作で
+// Widgetが動的に変更できたり状態管理ができるようになる
+class RandomWords extends StatefulWidget{
+  // StatefulWidget自身では状態を持たない。セットとなる、Sateクラスを継承する状態保持クラスで状態管理する
+  // StatefulWidgetでは自身の状態を管理するStateクラスのインスタンスを作成する
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+
+class RandomWordsState extends State<RandomWords>{
+  // Dartでは変数名やメソッド名の頭にアンダースコアをつけるかどうかでアクセスがprivateかpublicかを判定する
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generator'),),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){ // 無名関数を定義、引数にBuildContextと行番号を渡す
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
